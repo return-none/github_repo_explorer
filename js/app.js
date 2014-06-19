ac53845f9a279267e20a201dcd83ce6c60d02ceb
C1nde <secrypt@gmail.com>
2014-06-19 12:46:08 +0400

    charts

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
-var app = angular.module('myApp', ['ngGrid']);
+var app = angular.module('myApp', ['ngGrid', 'angularCharts']);
 
 app.controller('mainCtrl', function ($scope, $http) {
-    $scope.languages = [];
-    $scope.gridOptions = { data: 'languages' };
+    $scope.loaded = false;
+    $scope.languages_table = [];
+    $scope.languages_chart = [];
+
+    $scope.gridOptions = { data: 'languages_table' };
+    $scope.languages_chart_config = {
+        legend: {
+            display: true,
+            position: 'right'
+        }
+    };
+
     $http.get("https://api.github.com/repos/angular/angular.js/languages").success(function(data) {
-        $scope.languages = Object.keys(data).map(function(current) {
+        $scope.languages_table = Object.keys(data).map(function(current) {
             return {
                 name: current,
                 size: data[current]
             };
         });
+        $scope.languages_chart = {
+            data: $scope.languages_table.map(function(item) {
+                return { x: item.name, y: [item.size] };
+            })
+        };
+        $scope.loaded = true;
     });
 });
